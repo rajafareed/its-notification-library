9616b71255b73e6e4870a0b9a8ed2c7309852b93
rajajeee <41549157+rajajeee@users.noreply.github.com>
2025-01-27 12:03:39 +0500

    initial commit

---
+++ app/src/main/java/com/fe/sdkparentapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.fe.sdkparentapp;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.fe.mylibrary.NotificationModule;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        NotificationModule.initializeFirebase(MainActivity.this);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                NotificationModule.getFcmToken();
+//                NotificationModule.deviceFCMToken(MainActivity.this);
+            }
+        },1000);
+
+    }
+}
